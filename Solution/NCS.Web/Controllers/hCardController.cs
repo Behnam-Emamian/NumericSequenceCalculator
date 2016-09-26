9b4897b93f8d32c0482ad9a12d994314dd4c9f9d
Behnam Emamian <emamian@persianprocess.com>
2016-09-27 00:51:23 +1000

    AngularJS

---
+++ Solution/NCS.Web/Controllers/hCardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersianProcess.NCS.Web.Controllers
+{
+    public class hCardController : Controller
+    {
+        // GET: hCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
